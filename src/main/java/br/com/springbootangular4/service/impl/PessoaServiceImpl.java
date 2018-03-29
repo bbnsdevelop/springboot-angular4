ad6294bc3214885a75313154032012050e0d8d5b
Bruno Batista <brunno188@hotmail.com>
2018-03-28 22:41:07 -0300

    injeção de dependencia

--- src/main/java/br/com/springbootangular4/service/impl/PessoaServiceImpl.java
+++ src/main/java/br/com/springbootangular4/service/impl/PessoaServiceImpl.java
@@ -2,6 +2,7 @@ package br.com.springbootangular4.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.springbootangular4.builder.PessoaEntityBuilder;
@@ -14,6 +15,7 @@ import br.com.springbootangular4.service.PessoaService;
 @Service
 public class PessoaServiceImpl implements PessoaService{
 	
+	@Autowired
 	private PessoaRepository pessoaRepository;
 	 
 	@Override
